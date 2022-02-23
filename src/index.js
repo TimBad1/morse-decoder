8859a6a4f282a559ded771f7540e0f19977dcd0a
Тим Бад <timurbadalbaev19@gmail.com>
2022-02-23 23:30:00 +0300

    init: Morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,9 +37,46 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+// const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+
 function decode(expr) {
-    // write your solution here
+    let text = '';
+    let words = [];
+
+    for(let i = 0; i < expr.length; i+=2) {
+        switch (`${expr[i]}${expr[i + 1]}`) {
+            case '00': {text += '_'; break;}
+            case '10': {text += '.'; break;}
+            case '11': {text += '-'; break;}
+            case '**': {text += '*'; break;}
+            default: {console.log('error');}
+        }
+    }
+
+    while (text.length > 0) {
+        words.push(text.slice(0, 5))
+        text = text.slice(5);
+    }
+
+    for(let i = 0; i < words.length; i++) {
+        if(words[i] === '*****') {
+            words[i] = ' ';
+        }
+
+        for(let key in MORSE_TABLE) {
+            let newKey = '_____' + key;
+            
+            if(words[i] === newKey.slice(-5)) {
+                words[i] = MORSE_TABLE[key];
+                break;
+            }
+        }
+    }
+    // console.log(words.join(''));
+    return words.join('');
 }
+
+// decode(expr);
 
 module.exports = {
     decode
